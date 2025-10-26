c6dffdf6dbced3bae7b54b5102c09a599d5bf670
Silich <sili41995@gmail.com>
2025-10-26 11:36:03 +0200

    add VeteransSection

--- src/components/MainPage/VeteransSection/VeteransSection.styled.ts
+++ src/components/MainPage/VeteransSection/VeteransSection.styled.ts
@@ -1,3 +1,49 @@
 import styled from '@emotion/styled';
+import Azov from '@/icons/veterans/azov.svg?react';
+import { motion } from 'framer-motion';
 
-export const Section = styled.section``;
+export const Section = styled.section`
+  position: relative;
+`;
+
+export const BannerWrap = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  height: 100dvh;
+  overflow: hidden;
+`;
+
+export const Banner = styled(motion.img)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const Container = styled.div`
+  position: relative;
+  height: 300dvh;
+  background-color: #18181c;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+`;
+
+export const Content = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100dvh;
+`;
+
+export const Title = styled.h3``;
+
+export const TitlePart = styled.span``;
+
+export const AzovIconWrap = styled.span``;
+
+export const AzovIcon = styled(Azov)``;
+
+export const Accent = styled.span``;
--- src/components/MainPage/VeteransSection/VeteransSection.tsx
+++ src/components/MainPage/VeteransSection/VeteransSection.tsx
@@ -1,13 +1,70 @@
 import GeneralContainer from '@CommonComponents/GeneralContainer';
-import { FC } from 'react';
-import { Section } from './VeteransSection.styled';
+import { FC, useRef } from 'react';
+import {
+  Section,
+  AzovIcon,
+  AzovIconWrap,
+  Banner,
+  BannerWrap,
+  Container,
+  Content,
+  Title,
+  TitlePart,
+  Accent,
+} from './VeteransSection.styled';
+import SectionTitle from '@/components/common/SectionTitle';
+import banner from '@/images/veterans/banner.jpg';
+import {
+  SpringOptions,
+  useScroll,
+  useSpring,
+  useTransform,
+} from 'framer-motion';
 
 const VeteransSection: FC = () => {
+  const bannerContainerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: bannerContainerRef,
+    offset: ['start end', 'end start'],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1.5, 1]);
+
+  const transition: SpringOptions = {
+    stiffness: 150,
+    damping: 100,
+    mass: 0.5,
+  };
+
+  const smoothScale = useSpring(scale, transition);
+
   return (
     <Section>
-      <GeneralContainer>
-        <p>VeteransSection</p>
-      </GeneralContainer>
+      <SectionTitle text='Тренування для ветеранів' isHidden />
+
+      <BannerWrap ref={bannerContainerRef}>
+        <Banner src={banner} alt='Банер' style={{ scale: smoothScale }} />
+      </BannerWrap>
+
+      <Container>
+        <GeneralContainer>
+          <Content>
+            <Title>
+              <TitlePart>
+                <AzovIconWrap>
+                  <AzovIcon />
+                </AzovIconWrap>
+                <span>Безкоштовні</span>
+              </TitlePart>
+              <TitlePart>
+                <span>тренування</span>
+                <Accent>для</Accent>
+              </TitlePart>
+              <TitlePart>ветеранів</TitlePart>
+            </Title>
+          </Content>
+        </GeneralContainer>
+      </Container>
     </Section>
   );
 };
